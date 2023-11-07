85dd2483110fc056ffe1eef87f86eb0a1aa1cc07
Abhi <dgonedas@gmail.com>
2023-11-07 16:02:48 +0530

    giubgib

---
+++ tempo/src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import "./App.css"
+function App() {
+  const[dyna,setdyna]=useState('London')
+  const[data,setdata]=useState(null)
+
+
+
+  const clim=async()=>{
+    const reserve=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${dyna}&appid=a23a137c49937f27ec441b26b2d10823`)
+    const store=await reserve.json()
+    setdata(store.main)
+    console.log(store.main);
+  }
+
+
+  useEffect(()=>{
+    clim()
+  },[dyna])
+
+  const magic=(e)=>{
+    setdyna(e.target.value)
+  }
+
+
+
+
+  return (
+    <>
+    <h1 className='head'>Know Temparature</h1>
+    <div className='input-main'>
+    <input type="search" placeholder='city name' onChange={magic} value={dyna} className='input' />
+  
+    <h1 className='output'>{dyna}</h1>
+
+    {!data?(
+    <h3 className='err'>Result not Found!...</h3>
+   )
+  :(<>
+  <p className='res1'>Temp: {data.temp} K</p>
+  
+  <p className='res2'>Humidity: {data.humidity}</p>
+
+  </>
+    
+  )
+  
+
+  }
+    </div>
+    
+<div>
+
+</div>
+   
+    
+    </>
+  )
+}
+
+export default App
